838f5dbf272ec40ec0eaff55ddcaf50ce95f7110
seandong <sindon@gmail.com>
2021-10-24 12:17:31 +0800

    ADD: jsapi signature params

--- lib/lark/apis/jssdk.rb
+++ lib/lark/apis/jssdk.rb
@@ -4,6 +4,21 @@ module Lark
       def ticket_get
         post 'jssdk/ticket/get'
       end
+
+      def signature_params(url)
+        noncestr = SecureRandom.hex(4)
+        timestamp = Time.now.to_i
+        signature = Digest::SHA1.hexdigest(
+          "jsapi_ticket=#{jsapi_ticket}&noncestr=#{noncestr}&timestamp=#{timestamp}&url=#{url}"
+        )
+
+        {
+          url: url,
+          noncestr: noncestr,
+          timestamp: timestamp,
+          signature: signature
+        }
+      end
     end
   end
 end
