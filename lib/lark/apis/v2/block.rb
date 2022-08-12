fcf0d6bde99610967beb726845f4b2361a2409c3
David Ding <cod7ce@gmail.com>
2022-08-12 16:43:37 +0800

    FIX: change params as json

--- lib/lark/apis/v2/block.rb
+++ lib/lark/apis/v2/block.rb
@@ -5,7 +5,7 @@ module Lark
         def create(title:, block_type_id:, source_data:, source_meta:, version:)
           post 'block/v2/entities', {
             title: title, block_type_id: block_type_id,
-            source_data: source_data, source_meta: source_meta,
+            source_data: source_data.to_json, source_meta: source_meta.to_json,
             version: version
           }
         end
@@ -13,7 +13,7 @@ module Lark
         def update(block_id, version:, title: nil, block_type_id:, source_data:, source_meta:)
           put "block/v2/entities/#{block_id}", {
             title: title, block_type_id: block_type_id,
-            source_data: source_data, source_meta: source_meta,
+            source_data: source_data.to_json, source_meta: source_meta.to_json,
             version: version
           }.compact
         end
